c3f62041bc822e2f8ec3bfba85ec979c19e3624d
cecifernandez <mceciliafernandezst@gmail.com>
2024-12-12 14:06:50 -0300

    add spotify login

---
+++ Controllers/SpotifyController.cs
@@ -0,0 +1,40 @@
+﻿using APISocMed.DomainServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APISocMed.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpotifyController : ControllerBase
+    {
+        private readonly SpotifyService _spotifyService;
+
+        public SpotifyController(SpotifyService spotifyService)
+        {
+            _spotifyService = spotifyService;
+        }
+
+        [HttpGet("authorize")]
+        public IActionResult Authorize()
+        {
+            var url = _spotifyService.GetAuthorizationUrl();
+            //return Redirect(url);
+            return Ok(new { authorizationUrl = url });
+        }
+
+        [HttpGet("callback")]
+        public async Task<IActionResult> Callback([FromQuery] string code)
+        {
+            var tokenResponse = await _spotifyService.ExchangeCodeForToken(code);
+            return Ok(tokenResponse);
+        }
+
+        [HttpGet("me")]
+        public async Task<IActionResult> GetUserProfile([FromHeader] string accessToken)
+        {
+            var profile = await _spotifyService.GetUserProfile(accessToken);
+            return Ok(profile);
+        }
+    }
+}
---
+++ DomainServices/SpotifyService.cs
@@ -0,0 +1,58 @@
+﻿using System.Net.Http.Headers;
+
+namespace APISocMed.DomainServices
+{
+    public class SpotifyService
+    {
+        private readonly IConfiguration _configuration;
+
+        //access appsettings.json
+        public SpotifyService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GetAuthorizationUrl()
+        {
+            var clientId = _configuration["Spotify:clientId"]!;
+            var clientSecret = _configuration["Spotify:clientSecret"]!;
+            var redirectUri = _configuration["Spotify:redirectUri"]!;
+
+            var scopes = "user-read-private user-read-email";
+            return $"https://accounts.spotify.com/authorize?client_id={clientId}&response_type=code&redirect_uri={redirectUri}&scope={Uri.EscapeDataString(scopes)}";
+        }
+
+        public async Task<string> ExchangeCodeForToken(string code)
+        {
+            var clientId = _configuration["Spotify:clientId"]!;
+            var clientSecret = _configuration["Spotify:clientSecret"]!;
+            var redirectUri = _configuration["Spotify:redirectUri"]!;
+
+            using var client = new HttpClient();
+            var content = new FormUrlEncodedContent(new[]
+            {
+            new KeyValuePair<string, string>("grant_type", "authorization_code"),
+            new KeyValuePair<string, string>("code", code),
+            new KeyValuePair<string, string>("redirect_uri", redirectUri),
+            new KeyValuePair<string, string>("client_id", clientId),
+            new KeyValuePair<string, string>("client_secret", clientSecret),
+        });
+
+            var response = await client.PostAsync("https://accounts.spotify.com/api/token", content);
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody; 
+        }
+
+        public async Task<string> GetUserProfile(string accessToken)
+        {
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var response = await client.GetAsync("https://api.spotify.com/v1/me");
+            response.EnsureSuccessStatusCode();
+            var responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
+        }
+    }
+
+}
